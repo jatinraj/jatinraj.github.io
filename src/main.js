a43642cc3afc2ff74e6943472b0d877453ebf415
jatinraj <36581627+jatinraj@users.noreply.github.com>
2018-03-05 17:10:41 +0530

    t

--- src/main.js
+++ src/main.js
@@ -1,14 +1,7 @@
 import Vue from 'vue'
-import Vuetify from 'vuetify'
-import App from './App'
-import router from './router'
+import App from './App.vue'
 
-Vue.use(Vuetify)
-Vue.config.productionTip = false
-
-/* eslint-disable no-new */
 new Vue({
   el: '#app',
-  router,
   render: h => h(App)
 })
